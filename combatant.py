36491e2443579527b8a2ada8bf56a99f87edf02c
sadaraine <sadaraine@gmail.com>
2026-08-08 14:32:02 -0700

    Create main and combatant

---
+++ combatant.py
@@ -0,0 +1,6 @@
+class Combatant():
+    def __init__(self, actions: str, initiative: int, between: int, speed: int) -> None:
+	    actions = self.actions
+	    initiative = self.initiative
+	    between = self.between
+	    speed = self.speed
---
+++ main.py
@@ -0,0 +1,7 @@
+import sys
+from combatant import Combatant
+
+def main():
+    print("Welcome to the Initiative Tracker!\nChoose from the following:\nA: Add Combatant\nR: Remove Combatant\nE: Edit Combatant")
+if __name__ == "__main__":
+    main()
